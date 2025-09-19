9116d6417db5d7c3cbc70bf793deb2ebf263460c
Ted <ted@dbbuilder.io>
2025-09-19 02:05:31 -0700

    Restore Gmail readonly scope - Google OAuth permissions enabled

--- frontend/src/components/AddEmailModal.tsx
+++ frontend/src/components/AddEmailModal.tsx
@@ -126,7 +126,7 @@ export default function AddEmailModal({ isOpen, onClose, onEmailAdded }: AddEmai
         options: {
           redirectTo: `${window.location.origin}/auth/callback`,
           scopes: provider === 'google'
-            ? 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+            ? 'https://www.googleapis.com/auth/gmail.readonly'
             : 'https://graph.microsoft.com/mail.read'
         }
       })
